8ae25ae56ffb47017a1a63813f57c672d3fab79a
YIDONG ZHU <donyidong@hotmail.com>
2022-09-30 22:43:25 +0200

    Update task06.py

--- Assignment4/ralonsosirera-190182/task06.py
+++ Assignment4/ralonsosirera-190182/task06.py
@@ -10,7 +10,7 @@ Original file is located at
 """
 
 !pip install rdflib 
-github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2022-2023/master/Assignment4"
+github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2020-2021/master/Assignment4"
 
 """Leemos el fichero RDF de la forma que lo hemos venido haciendo"""
 
