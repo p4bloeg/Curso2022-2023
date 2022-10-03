370013e2110d3aceaed6180c04a7d49e9608993f
Atlas1225 <62561079+Atlas1225@users.noreply.github.com>
2022-10-03 10:06:25 +0200

    Update task07.py

--- Assignment4/Atlas1225-b190154/task07.py
+++ Assignment4/Atlas1225-b190154/task07.py
@@ -26,8 +26,12 @@ g.parse(github_storage+"/rdf/example6.rdf", format="xml")
 # **TASK 7.1: List all subclasses of "Person" with RDFLib and SPARQL**
 
 # In[8]:
+#RDFlib:
+ns = Namespace("http://somewhere#")
+for s,p,o in g.triples((None, RDFS.subClassOf, ns.Person)):
+  print(s)
 
-
+#SPARQL:
 q1 = """
 select ?s where{
 ?s rdfs:subClassOf ns:Person
@@ -44,10 +48,18 @@ for r in g.query(q1):
 
 # In[11]:
 
+# RDFlib:
+for s, p, o in g.triples((None, RDF.type, ns.Person)):
+  print(s)
+for s, p, o in g.triples((None, RDFS.subClassOf, ns.Person)):
+  for s1, p1, o1 in g.triples((None, RDF.type, s)):
+    print(s1)
 
+#SPARQL:
 q1 = """
 select ?s where{
-?s rdf:type ns:Person
+?s rdf:type ns:Person.
+?s rdfs:subClassOf ns:Person.
 }
 """
 # Visualize the results
@@ -61,10 +73,21 @@ for r in g.query(q1):
 
 # In[19]:
 
+#RDFlib:
+for s, p, o in g.triples((None, RDF.type, ns.Person)):
+  for s1, p1, o1 in g.triples((s, None, None)):
+    print(s1, p1, o1)
+for s, p, o in g.triples((None, RDFS.subClassOf, ns.Person)):
+  for s1, p1, o1 in g.triples((None, RDF.type, s)):
+    for s2, p2, o2 in g.triples((s1, None, None)):
+      print(s2, p2, o2)
 
+
+#SPARL:
 q1 = """
 select ?s ?p where{
-?s rdf:type ns:Person.
+?subc rdfs:subClassOf ns:Person.
+?s rdf:type ?subc.
 ?s ?x ?p
 }
 """
